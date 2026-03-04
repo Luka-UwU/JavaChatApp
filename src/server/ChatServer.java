ff4450ef4bccbb9e3894a7a9251c5dac5faf1f42
Luka <luca.tataru@protonmail.com>
2026-03-04 22:53:27 +0200

    Introduction to Server-Client Architecture

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -2,14 +2,67 @@ package server;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class ChatServer {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(2000);
-        //Port number 2000
-        System.out.println("Server started. Listening on port 2000...");
+    //List to keep track of all connected clients
+    private static List<ClientHandler> clients = new ArrayList<>();
 
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("Client connected.");
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(9001);
+        //Port number 9001
+        System.out.println("Server started. Listening on port 9001...");
+
+        while(true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected.");
+
+            ClientHandler clientThread = new ClientHandler(clientSocket, clients);
+            clients.add(clientThread);
+            new Thread(clientThread).start();
+        }
+
+
     }
+
+    class ClientHandler implements Runnable{
+
+        private Socket clientSocket;
+        private List<ClientHandler> clients;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        public ClientHandler(Socket clientSocket, List<ClientHandler> clients) throws IOException{
+            this.clientSocket = clientSocket;
+            this.clients = clients;
+            this.out = new PrintWriter(clientSocket.getOutputStream(), true);
+            this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        }
+
+        public void run() {
+           try{
+               String inputLine;
+               while ((inputLine = in.readLine()) != null)
+               {
+                   //Broadcast message to all clients
+                   for(ClientHandler aClient : clients){ //for each clientHandler object in the clients list
+                       aClient.out.println(inputLine);
+                   }
+
+               }
+           } catch (IOException e){
+               System.out.println("An error occurred: " + e.getMessage());
+           }
+           finally {
+               try{
+                   in.close();
+                   out.close();
+               } catch (IOException e){
+                   e.printStackTrace();
+               }
+
+           }
+        }
+    }
+
 }
