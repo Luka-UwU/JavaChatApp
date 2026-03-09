cb072d74aa8fc004b09445615b01da6a86753596
Luka <luca.tataru@protonmail.com>
2026-03-09 18:02:46 +0200

    Created startServer method and server constructor. Moved related code from main.

--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -7,25 +7,50 @@ import java.util.*;
 public class ChatServer {
     //List to keep track of all connected clients
     private static List<ClientHandler> clients = new ArrayList<>();
+    private ServerSocket serverSocket;
+
+    public ChatServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(9001);
+
+        ChatServer chatServer = new ChatServer(9001);
         //Port number 9001
-        System.out.println("Server started. Listening on port 9001...");
-
-        while(true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Client connected.");
-
-            ClientHandler clientThread = new ClientHandler(clientSocket, clients);
-            clients.add(clientThread);
-            new Thread(clientThread).start();
-        }
-
+        chatServer.startServer();
 
     }
 
-    static class ClientHandler implements Runnable{
+    public void startServer(){
+        try {
+            System.out.println("Server started. Listening on port 9001...");
+
+            while (!serverSocket.isClosed()) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("A new client has connected.");
+
+                ClientHandler clientThread = new ClientHandler(clientSocket, clients);
+                clients.add(clientThread);
+                Thread thread = new Thread(clientThread);
+                thread.start();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred when trying to start the server.");
+        }
+    }
+
+    public void stopServer(){
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    class ClientHandler implements Runnable{
 
         private Socket clientSocket;
         private List<ClientHandler> clients;
