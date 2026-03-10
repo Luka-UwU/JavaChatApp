a4120f47f8c65ed9b449123a896919815f26b760
Luka <luca.tataru@protonmail.com>
2026-03-10 14:55:28 +0200

    Implemented usernames and created broadcastMessage and removeClient methods.

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -4,36 +4,37 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable{
 
     private Socket clientSocket;
-    private List<ClientHandler> clients;
+    //List to keep track of all connected clients
+    public static List<ClientHandler> clients = new ArrayList<>();
     private PrintWriter out;
     private BufferedReader in;
+    private String clientUsername;
 
-    public ClientHandler(Socket clientSocket, List<ClientHandler> clients) throws IOException {
+    public ClientHandler(Socket clientSocket) throws IOException {
         this.clientSocket = clientSocket;
-        this.clients = clients;
         this.out = new PrintWriter(clientSocket.getOutputStream(), true);
-        this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); //receive message from client
+        this.clientUsername = in.readLine();
+        ClientHandler.clients.add(this);
+        broadcastMessage("Server: " + clientUsername + " has joined the chat.");
     }
 
     public void run() {
         try{
             String inputLine;
-            while ((inputLine = in.readLine()) != null)
-            {
-                //Broadcast message to all clients
-                for(ClientHandler aClient : clients){ //for each clientHandler object in the clients list
-                    aClient.out.println(inputLine);
-                }
-
+            while(clientSocket.isConnected()) {
+                inputLine = in.readLine();
+                broadcastMessage(inputLine);
             }
         } catch (IOException e){
             System.out.println("An error occurred: " + e.getMessage());
         }
         finally {
             try{
+                removeClient();
                 in.close();
                 out.close();
             } catch (IOException e){
@@ -41,5 +42,21 @@ class ClientHandler implements Runnable{
             }
 
         }
+    }
+
+    public void broadcastMessage(String messageToSend){
+        //Broadcast message to all clients
+        for (ClientHandler aClient : clients) { //for each clientHandler object in the clients list
+
+            if (!aClient.clientUsername.equals(clientUsername)) {
+                aClient.out.println(messageToSend);
+                //aClient.out.flush();
+            }
+        }
+    }
+
+    public void removeClient(){
+        clients.remove(this);
+        broadcastMessage("Server: " + clientUsername + " has left the chat.");
     }
 }
